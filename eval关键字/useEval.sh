a95f7c2e75acc684dad94fcb1da4516302e11b14
mac <847118663@qq.com>
2020-10-14 12:20:03 +0800

    eval的使用

--- eval关键字/useEval.sh
+++ eval关键字/useEval.sh
@@ -1,6 +1,16 @@
 #/bin/bash
 
-myfile="cat dirname $0"
-eval eval echo $myfile
-echo $myfile
-
+dir="pwd"
+myfile="cat  $0"
+#1 无意义
+eval eval  $myfile
+#2
+echo  $myfile
+#3
+echo  pwd
+#4
+echo echo  pwd
+$5
+eval  pwd
+#6 无意义 嵌套eval 
+eval eval  pwd

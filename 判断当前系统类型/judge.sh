7efc577c6e77cf1da92dbc6abab40e124384beb4
mac <847118663@qq.com>
2020-10-14 16:33:25 +0800

    case的学习

---
+++ 判断当前系统类型/judge.sh
@@ -0,0 +1,9 @@
+case "$OSTYPE" in
+  solaris* echo "SOLARIS" ;
+  darwin*  echo "OSX" ; 
+  linux*   echo "LINUX" ;
+  bsd*    echo "BSD" ;
+  msys*    echo "WINDOWS" ;
+  *       echo "unknown: $OSTYPE" ;
+esac
+
